1963886eb460092096041981cd610d57a5581083
yangkaile <guyexing@foxmail.com>
2018-11-30 16:07:42 +0800

    添加查询日志列表

--- src/views/m_log/LogonLog.vue
+++ src/views/m_log/LogonLog.vue
@@ -8,25 +8,34 @@
 					<el-input v-model="filters.name" placeholder="姓名"></el-input>
 				</el-form-item>
 				<el-form-item>
-					<el-button type="primary" v-on:click="getUsers">查询</el-button>
+					<el-button type="primary" v-on:click="getDataList">查询</el-button>
 				</el-form-item>
 			</el-form>
 		 </el-col>
 		 <el-col :span="12" class="toolbar">
-			<el-pagination layout="prev, pager, next" @current-change="handleCurrentChange" :page-size="20" :total="total" style="float:right;">
+			<el-pagination layout="prev, pager, next" @current-change="handleCurrentChange" :page-size="pageSize" :total="total" style="float:right;">
 			</el-pagination>
 		 </el-col>
 		</el-row>
-
+                
 		<!--列表-->
-		<el-table :data="routers" highlight-current-row v-loading="listLoading" @selection-change="selsChange" style="width: 100%;">
-			<el-table-column prop="name" label="接口名称" sortable>
+		<el-table :data="dataList" highlight-current-row v-loading="listLoading"  style="width: 100%;">
+			<el-table-column prop="userId" label="用户ID"  sortable>
 			</el-table-column>
-			<el-table-column prop="requestType" width="200" label="请求方式" >
+			<el-table-column prop="type" width="120px" label="登录/登出" sortable>
 			</el-table-column>
-			<el-table-column prop="routerUrl" label="请求地址" >
+			<el-table-column prop="ip"   label="IP地址" >
 			</el-table-column>
-			<el-table-column prop="serviceName" label="组件名" >
+			<el-table-column prop="way"   label="登录方式" >
+			</el-table-column>
+			<el-table-column prop="createTime"  label="操作时间" sortable>
+				<template slot-scope="scope">
+					<el-date-picker 
+						disabled
+						v-model="scope.row.createTime"
+						type="datetime">
+					</el-date-picker>
+				</template>
 			</el-table-column>
 		</el-table>
 	</section>
@@ -34,7 +43,7 @@
 
 <script>
 	import util from '../../common/js/util'
-	import { req_getRouterList} from '../../api/api';
+	import {req_getLogonLogList} from '../../api/api';
 
 	export default {
 		data() {
@@ -42,54 +51,38 @@
 				filters: {
 					name: ''
 				},
-				routers: [],
+				dataList: [],
 				total: 0,
 				page: 1,
-				listLoading: false,
-				sels: [],//列表选中列
-
-				editFormVisible: false,//编辑界面是否显示
-				editLoading: false,
-				editFormRules: {
-					name: [
-						{ required: true, message: '请输入姓名', trigger: 'blur' }
-					]
-				},
-				addLoading: false,
-				addFormRules: {
-					name: [
-						{ required: true, message: '请输入姓名', trigger: 'blur' }
-					]
-				}
+				pageSize: 10,
+				listLoading: false
 			}
 		},
 		methods: {
 			handleCurrentChange(val) {
 				this.page = val;
-				this.getUsers();
+				this.getDataList();
 			},
-			//获取用户列表
-			getUsers() {
+			//获取登录日志列表
+			getDataList() {
 				let para = {
 					page: this.page,
 					name: this.filters.name
 				};
 				this.listLoading = true;
-				req_getRouterList().then((res) => {
-					console.log('req_getRouterList',res);
-					//TODO 分页查询
-					this.total = 100;
-					this.routers = res.data;
+				req_getLogonLogList(this.page,this.pageSize).then((res) => {
+					console.log('req_getLogonLogList',res);
+					let response = res.data;
+					if(response.total != 0){
+						this.total = response.total;
+					}
+					this.dataList = response.data;
 					this.listLoading = false;
 				});
-			},
-			selsChange: function (sels) {
-				this.sels = sels;
-			},
-
+			}
 		},
 		mounted() {
-			this.getUsers();
+			this.getDataList();
 		}
 	}
 
